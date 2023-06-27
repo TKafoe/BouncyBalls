574ccc34aad2ef125b87210168d1d35acc2edac5
TKafoe <kafoe.tom@gmail.com>
2023-06-27 14:08:40 +0200

    Add collision to the balls

--- src/js/draw.js
+++ src/js/draw.js
@@ -2,25 +2,73 @@ import * as paper from 'paper';
 
 class RotatingRectangle {
   ABSORPTION = 0.7;
+  FRICTION = 0.005; // Friction coefficient
 
-  simulateGravity(event) {
-    this.v += this.a * (event.delta * 10);
-    this.pos = this.pos.add([0, this.v * event.delta]);
+  simulateGravity(event, balls) {
+    const deltaTime = event.delta;
+    this.v = this.v.add(this.a.multiply(deltaTime * 10));
+    this.pos = this.pos.add(this.v.multiply(deltaTime));
 
     if (this.pos.y + this.radius > this.floor_height) {
-      this.v = -1 * this.v * this.ABSORPTION;
+      this.v.y = -1 * this.v.y * this.ABSORPTION;
       this.pos.y = this.floor_height - this.radius;
     }
+
+    // Apply friction to the horizontal velocity
+    this.v.x *= 1 - this.FRICTION;
+
+    // Collision with walls
+    if (this.pos.x - this.radius < 0) {
+      this.v.x = Math.abs(this.v.x);
+      this.pos.x = this.radius;
+    } else if (this.pos.x + this.radius > paper.view.size.width) {
+      this.v.x = -Math.abs(this.v.x);
+      this.pos.x = paper.view.size.width - this.radius;
+    }
+
+    this.handleCollisions(balls);
 
     this.path.position = this.pos;
   }
 
+  handleCollisions(balls) {
+    for (let i = 0; i < balls.length; i++) {
+      const ball = balls[i];
+      if (ball !== this) {
+        const distance = this.pos.getDistance(ball.pos);
+        if (distance <= this.radius + ball.radius) {
+          const collisionNormal = this.pos.subtract(ball.pos).normalize();
+          const relativeVelocity = this.v.subtract(ball.v);
+          const impulse =
+              (2 * relativeVelocity.dot(collisionNormal)) /
+              (1 / this.mass + 1 / ball.mass);
+
+          const collisionImpulse = collisionNormal.multiply(impulse);
+
+          this.v = this.v.subtract(collisionImpulse.divide(this.mass));
+          ball.v = ball.v.add(collisionImpulse.divide(ball.mass));
+
+          const separationVector = collisionNormal.multiply(
+              (this.radius + ball.radius - distance) / 2
+          );
+          this.pos = this.pos.add(separationVector);
+          ball.pos = ball.pos.subtract(separationVector);
+        }
+      }
+    }
+  }
+
   constructor(x_start, y_start, floor_height, ball_size) {
     this.floor_height = floor_height;
-    this.v = -2000 * Math.random() + -500;
-    this.a = 9.81 * 10;
+    const speed = Math.random() * (2000 - 500) + 500;
+    this.v = new paper.Point(
+        Math.random() < 0.5 ? -speed : speed,
+        -2000 * Math.random() + -500
+    );
+    this.a = new paper.Point(0, 9.81 * 10);
     this.pos = new paper.Point(x_start, y_start);
     this.radius = ball_size;
+    this.mass = Math.PI * this.radius * this.radius;
 
     this.path = new paper.Path.Circle({
       center: this.pos,
@@ -29,17 +77,14 @@ class RotatingRectangle {
       fillColor: paper.Color.random(),
     });
 
-    this.path.onFrame = (event) => this.simulateGravity(event);
+    this.path.onFrame = (event) => this.simulateGravity(event, this.balls);
   }
 }
 
-// Only executed our code once the DOM is ready.
-window.onload = function() {
+window.onload = function () {
   var canvas = document.getElementById('myCanvas');
-
-  // Create an empty project and a view for the canvas:
   paper.setup(canvas);
-  // Create a Paper.js Path to draw a line into it:
+
   let BALL_SIZE = 10;
   let WIDTH = canvas.width;
   let HEIGHT = canvas.height;
@@ -49,18 +94,32 @@ window.onload = function() {
     to: [WIDTH, FLOOR_HEIGHT],
     strokeColor: 'black',
   });
+  new paper.Path.Line({
+    from: [0, 0],
+    to: [0, HEIGHT],
+    strokeColor: 'black',
+  });
+  new paper.Path.Line({
+    from: [WIDTH, 0],
+    to: [WIDTH, HEIGHT],
+    strokeColor: 'black',
+  });
 
   let num_balls = 100;
+  let balls = [];
   for (let i = 0; i < num_balls; i++) {
-    new RotatingRectangle(
-      (Math.random() * ((WIDTH - (BALL_SIZE)) - (BALL_SIZE)) + (BALL_SIZE)),
-      // (Math.random() * (HEIGHT * 0.2 - (10)) + 10),
-      FLOOR_HEIGHT - 50,
-      FLOOR_HEIGHT,
-      BALL_SIZE
+    const ball = new RotatingRectangle(
+        Math.random() * (WIDTH - BALL_SIZE) + BALL_SIZE,
+        FLOOR_HEIGHT - 50,
+        FLOOR_HEIGHT,
+        BALL_SIZE
     );
+    balls.push(ball);
   }
 
-  // Draw the view now:
+  balls.forEach((ball) => {
+    ball.balls = balls;
+  });
+
   paper.view.draw();
-}
+};
